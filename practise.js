183fbdc379c7e3051bf6077bb2e6e682bd70af68
imran2024bd <imranitbd34@gmail.com>
2024-01-28 01:45:14 +0600

    practise task start

---
+++ practise.js
@@ -0,0 +1,30 @@
+// start from 28.01.2024  module = 17_4
+const salary = 25001;
+const isBcs = true;
+const height = 69;
+
+// ------------------------------------------------
+// if (salary > 20000 && height > 66) {
+//     console.log(" good");
+// }
+// else {
+//     console.log(" next");
+// }
+
+// -------------------------------------------------
+
+// if (salary > 25000 || height > 66) {
+//         console.log(" good");
+//     }
+//     else {
+//         console.log(" next");
+//     }
+
+// ----------------------------------------------------------?
+if (salary > 25000 || height > 66 || isBcs == true) {
+    console.log(" good");
+}
+else {
+    console.log(" next");
+}
+
