59597bbfb380c84997f8f2604494d92b7985ab9d
imran2024bd <imranitbd34@gmail.com>
2024-01-28 04:36:15 +0600

    Array Structure of incluude, isarray, index off is complete

--- practise.js
+++ practise.js
@@ -52,7 +52,36 @@
 var number = [10, 20, 30, 40, 50, 55];
 //index    = [0 ,  1   2   3   4   5]
 // to find index
-console.log(number.indexOf(40));
-console.log(number.indexOf(55));
-// To find  number of specified index
-console.log(number[2]);
+// console.log(number.indexOf(40));
+// console.log(number.indexOf(55));
+// To find  number / value in an array of specified index
+// console.log(number[2]);
+// to find how many index number include in an array 
+// console.log(number.length);
+// to replace any value of an Array by index number
+// number[5] = 60;
+// console.log(number);
+// To find index number  in an array of specified value
+// console.log(number.indexOf(40));
+ // To find  number in the array of a specified index
+// console.log(number[2]);
+// To add value in an array in last positioning 
+// number.push(70,80,90,100);
+// console.log(number);
+// To Delete value in an array in last positioning
+// number.pop(55);
+// console.log(number);
+// To Delete value in an array in first positioning
+// number.shift();
+// console.log(number);
+// To add value in an array in first positioning
+// number.unshift();
+// console.log(number);
+// Determines whether an array includes a certain element, returning true or false as appropriate.
+// console.log(number.includes(70));
+// console.log(number.indexOf(50));
+// to find isArray
+// console.log(Array.isArray(number));
+// var age = 15;
+// console.log(Array.isArray(age));
+
